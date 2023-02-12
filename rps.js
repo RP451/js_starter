83138d36728c9fb98148c55b5be5270764082520
rahul <pandarahul451@gmail.com>
2023-02-12 20:08:58 +0530

    rps with nested if statements and multiple console logs for troubleshooting

--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-const playerSelection = '';
+const playerSelection = "rock";
 const computerSelection = getComputerSelection();
 
 function getComputerSelection()
@@ -6,5 +6,70 @@ function getComputerSelection()
     const comp_options = ['rock','paper','scissors'];
     return comp_options[Math.floor(Math.random()*comp_options.length)];
 }
+console.log(computerSelection);
+console.log(playerSelection);
 
-console.log(getComputerSelection());
+function playRound(computerSelection, playerSelection)
+{
+    if(computerSelection === playerSelection)
+    {
+        console.log(1);
+        return("It's a tie !");
+    }
+    else if(computerSelection === 'rock')
+    {
+        if(playerSelection === 'scissors')
+        {
+            console.log(2);
+            return("Computer wins !");
+        }
+        else if(playerSelection === 'paper')
+        {
+            console.log(3);
+            return("Player wins !");
+        }
+        else
+        {
+            console.log(4);
+            return("It's a tie");
+        }
+    }
+    else if(computerSelection === 'paper')
+    {
+        if(playerSelection === 'rock')
+        {
+            console.log(5);
+            return("Computer wins !");
+        }
+        else if(playerSelection === 'scissors')
+        {
+            console.log(6);
+            return("Player wins !");
+        }
+        else
+        {
+            console.log(7);
+            return("It's a tie !");
+        }
+    }
+    else if(computerSelection === 'scissors')
+    {
+        if(playerSelection === 'rock')
+        {
+            console.log(8);
+            return("Player wins !");
+        }
+        else if(playerSelection === 'paper')
+        {
+            console.log(9);
+            return("Computer wins !");
+        }
+        else
+        {
+            console.log(10);
+            return("It's a tie !");
+        }
+    }
+}
+
+console.log(playRound(computerSelection, playerSelection));
