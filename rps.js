554bafa5f90cc991f1ae120f9723b6c951fcf65e
rahul <pandarahul451@gmail.com>
2023-05-21 20:11:27 +0530

    Refactoring code to remove multiple nested if statements

--- rps.js
+++ rps.js
@@ -6,8 +6,8 @@ function getComputerSelection()
     const comp_options = ['rock','paper','scissors'];
     return comp_options[Math.floor(Math.random()*comp_options.length)];
 }
-console.log(computerSelection);
-console.log(playerSelection);
+console.log("computer plays"+computerSelection);
+console.log("player plays"+playerSelection);
 
 function playRound(computerSelection, playerSelection)
 {
@@ -16,59 +16,15 @@ function playRound(computerSelection, playerSelection)
         console.log(1);
         return("It's a tie !");
     }
-    else if(computerSelection === 'rock')
+    else if(computerSelection === 'rock' && playerSelection ==='scissors' || computerSelection === 'scissors' && playerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'rock')
     {
-        if(playerSelection === 'scissors')
-        {
-            console.log(2);
-            return("Computer wins !");
-        }
-        else if(playerSelection === 'paper')
-        {
-            console.log(3);
-            return("Player wins !");
-        }
-        else
-        {
-            console.log(4);
-            return("It's a tie");
-        }
+        console.log(2);
+        return("Computer wins !");
     }
-    else if(computerSelection === 'paper')
+    else if(playerSelection === 'rock' && computerSelection ==='scissors' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock')
     {
-        if(playerSelection === 'rock')
-        {
-            console.log(5);
-            return("Computer wins !");
-        }
-        else if(playerSelection === 'scissors')
-        {
-            console.log(6);
-            return("Player wins !");
-        }
-        else
-        {
-            console.log(7);
-            return("It's a tie !");
-        }
-    }
-    else if(computerSelection === 'scissors')
-    {
-        if(playerSelection === 'rock')
-        {
-            console.log(8);
-            return("Player wins !");
-        }
-        else if(playerSelection === 'paper')
-        {
-            console.log(9);
-            return("Computer wins !");
-        }
-        else
-        {
-            console.log(10);
-            return("It's a tie !");
-        }
+        console.log(3);
+        return("Player wins !");
     }
 }
 
